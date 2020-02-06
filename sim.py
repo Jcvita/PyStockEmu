f6ebca70939e42d9d186dbe630361aac685a0cdb
Joe Vita <jcvita2012@gmail.com>
2020-02-06 01:05:08 -0500

    portfolio

--- sim.py
+++ sim.py
@@ -4,52 +4,58 @@
 
 from dataclasses import dataclass
 
+
+@dataclass
+class Portfolio:
+
+    def __init__(self):
+        self.value = 0
+        self.stocks = []
+
+
 @dataclass
 class Agent:
-    def __init__(self, m, p):
-        self.money = m
-        self.portfolio = p
+
+    money = None
+    portfolio = None
+
+    def __init__(self):
+        self.money = 0
+        self.portfolio = Portfolio
 
     def initialize(self, m=100000):
         self.money = m
         self.portfolio = []
-    
+
     def buy(self, stock):
         self.money -= stock.price
         self.portfolio.append(stock.name)
-    
+
     def sell(self, stock):
-        if stock.name in portfolio:
+        if stock.name in self.portfolio:
             self.money += stock.price
             self.portfolio.remove(stock.name)
-        
+
+    def reset(self):
+        self.money = 0
+        self.portfolio = Portfolio
+
 
 @dataclass
 class Stock:
+
     def __init__(self, n, p):
         self.name = n
         self.price = p
 
+    def update_price(self, p):
+        self.price = p
 
-""" 
 
-def initialize(agent, startamt, realtime = False):
-    if isinstance(agent, Agent):
-        agent.money = startamt
-        agent.portfolio = []
-    return agent
-
-def buy(agent, stock):
-    if isinstance(agent, Agent) and isinstance(stock, Stock):
-        agent.money -= stock.price
-    else:
-        print('Cannot buy. Type error')
-"""
-
-joe = Agent(0,0)
-msft = Stock(0,0)
+joe = Agent
+msft = Stock(0, 0)
 msft.name = 'msft'
-msft.price = 69.0
+msft.update_price(69.0)
 joe.initialize(690000)
 joe.buy(msft)
 print(joe.money)
